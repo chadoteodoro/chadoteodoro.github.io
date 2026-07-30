3ac30647237461ce1b674100b3267b584a142094
KaykyOne <kaykyzioti@gmail.com>
2026-07-30 15:03:27 -0300

    fiz

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 window.TEODORO_CONFIG = {
   eventDate: "2026-10-17T15:00:00-03:00",
   eventStartLabel: "17 de outubro de 2026 às 15h",
-  address: "R. Dr. Bolívar, 382 - Ponto Chic, Nova Iguaçu - RJ, 26030-480",
+  address: "R. Dr. Bolívar, 384 - Ponto Chic, Nova Iguaçu - RJ, 26030-480",
 
   // Coloque o número do responsável com DDI + DDD, apenas dígitos.
   // Exemplo: "5511999999999"
